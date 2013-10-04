044c614ceff1f8969a4878fafcc2d6f715c107e4
patrick brisbin <pbrisbin@gmail.com>
2013-10-04 19:20:25 -0400

    Implement shelling out

--- Yak/Liquid.hs
+++ Yak/Liquid.hs
@@ -1,7 +1,10 @@
 module Yak.Liquid (liquid) where
 
 import Data.Text (Text)
+import System.Process (readProcess)
 
--- TODO: echo content | liquid context
+import qualified Data.Text as T
+
 liquid :: Text -> Text -> IO Text
-liquid template context = return template
+liquid template context = fmap T.pack
+                        $ readProcess "liquid" [T.unpack context] (T.unpack template)
--- Yak/Pandoc.hs
+++ Yak/Pandoc.hs
@@ -1,7 +1,10 @@
 module Yak.Pandoc (pandoc) where
 
 import Data.Text (Text)
+import System.Process (readProcess)
 
--- TODO: echo content | pandoc --from=markdown --to=html or use Pandoc
+import qualified Data.Text as T
+
 pandoc :: Text -> IO Text
-pandoc markdown = return markdown
+pandoc markdown = fmap T.pack
+                $ readProcess "pandoc" ["--from=markdown", "--to=html"] (T.unpack markdown)
